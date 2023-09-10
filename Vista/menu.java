7c8145138c12a2c5ca1439f2a7aa50d59ffc1067
JesusTapiaMartin <jesustapiamartin@gmail.com>
2023-09-09 23:55:08 -0300

    Interfaz del sistema de matrícula estudiantil 3

--- Vista/menu.java
+++ Vista/menu.java
@@ -9,7 +9,7 @@ public class menu {
         do {
             System.out.println("========================================");
             System.out.println("    Sistema de Matrícula Estudiantil    ");
-            System.out.println("                 S.M.E                  ");
+            System.out.println("               La Granja                ");
             System.out.println("========================================");
             System.out.println("        [1] Ingresar alumno             ");
             System.out.println("        [2] Mostrar alumnos             ");
@@ -20,6 +20,7 @@ public class menu {
             System.out.println("========================================");
             System.out.print  ("            Ingrese su opción  :        ");
             opcion = lector.nextLine();
+            System.out.println("========================================");
 
             switch (opcion){
                 case "1":
@@ -38,10 +39,10 @@ public class menu {
                     break;
 
                 case "6":
-                    System.out.println("Hasta luego :)");
+                    System.out.println("        Hasta luego... ");
 
                 default:
-                    System.out.println("Ingrese una opción valida");
+                    System.out.println("        Ingrese una opción válida... ");
                         break;
             }
         } while ( !opcion.equals("6") );
