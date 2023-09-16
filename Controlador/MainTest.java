0090d1ac6028addf98040a3a690185cf52638a4e
JesusTapiaMartin <jesustapiamartin@gmail.com>
2023-09-15 23:42:48 -0300

    Pruebas unitarias

--- Controlador/MainTest.java
+++ Controlador/MainTest.java
@@ -12,17 +12,20 @@ class MainTest {
     @Test
     void testAgregarAlumnoCSV() {
         Alumno nuevoAlumno = new Alumno(
-                "Juan"          , "Pérez"               , "18"              , "12345678-9"      , "2023001"         , "Masculino"   , "Calle 123"   ,
-                "Ciudad A"      , "juan@example.com"    , "Chileno"         , "Padres"          , "Etnia X"         , "Colegio A"   ,
-                "8"             , "No"                  , "Ninguna"         , "Ninguno"         , "01/01/2005"      , "+56912345678",
-                "Primero"       , "A"                   , "a"
+                "Juan"      , "Pérez"               , "18"          , "12345678-9"  , "2023001"         , "Masculino"       , "Calle 123"   ,
+                "Ciudad A"  , "juan@example.com"    , "Chileno"     , "Padres"      , "Etnia X"         , "Colegio A"       ,
+                "8"         , "No"                  , "Ninguna"     , "Ninguno"     , "01/01/2005"      , "+56912345678"    ,
+                "Primero"   , "A"                   , "A"
         );
 
-        Main.agregarAlumno(nuevoAlumno);
+        CSV.agregarAlumnoCSV(nuevoAlumno);
+
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        Main.mostrarAlumnos();
-        assertTrue(outContent.toString().contains(" Alumno registrado correctamente... "));
+
+        CSV.mostrarAlumnosCSV();
+
+        assertTrue(outContent.toString().contains("     El alumno y su apoderado registrados correctamente "));
     }
 
     @Test
@@ -38,22 +41,22 @@ class MainTest {
     void testMostrarAlumnoPorRut() {
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        String rutBusqueda = "12345678-9";
+        String rutBusqueda      = "12345678-9";
         Main.mostrarAlumnoPorRut(rutBusqueda);
-        String expectedOutput = "Rut alumno                        : 12345678-9";
+        String expectedOutput   = "Rut alumno                        : 12345678-9";
         assertTrue(outContent.toString().contains(expectedOutput));
     }
 
     @Test
     void testEliminarAlumnoCSV() {
-        String rutAEliminar = "12345678-9";
+        String rutAEliminar     = "12345678-9";
         Main.eliminarAlumnoCSV(rutAEliminar);
         // Verificar que el estudiante se haya eliminado correctamente del archivo
     }
 
     @Test
     void testModificarAlumnoPorRut() {
-        String rutAModificar = "12345678-9";
+        String rutAModificar    = "12345678-9";
         Main.modificarAlumnoPorRut(rutAModificar);
         // Verificar que los datos del estudiante se hayan modificado correctamente en el archivo
     }
